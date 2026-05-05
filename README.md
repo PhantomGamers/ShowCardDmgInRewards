28dcbc7e7f169d14beb5658d52d7c1943b7a9a50
Chris Yeninas <844685+PhantomGamers@users.noreply.github.com>
2026-05-05 16:14:16 -0400

    init

---
+++ README.md
@@ -0,0 +1,25 @@
+# ShowCardDmgInRewards for Slay the Spire 2
+
+Show how much dmg a card would do before picking it if it depends on other cards in your deck in the game Slay the Spire 2  
+
+## Installation
+
+### Vortex
+
+ 1. Go to the [NexusMods listing for the mod](https://www.nexusmods.com/slaythespire2/mods/800?tab=files)
+ 2. Click "Mod manager download"
+ 3. Enjoy
+
+### Manual
+
+ 1. Go to your Slay the Spire 2 install folder, this is the folder that contains the game executable as well as the `data_sts2_{platform}` folder.
+ 2. Create a folder named `mods`
+ 3. Download the [latest release](https://github.com/phantomgamers/ShowCardDmgInRewards/releases/latest/download/ShowCardDmgInRewards.zip)
+ 4. Extract it to the mods folder so it looks like the following:
+ ```
+📂Slay the Spire 2  
+ ┣ 📂mods  
+ ┃ ┗📜ShowCardDmgInRewards
+ ┃   ┗📦ShowCardDmgInRewards.dll
+ ┃   ┗🖹 ShowCardDmgInRewards.json
+ ```
---
+++ ShowCardDmgInRewardsCode/Patches.cs
@@ -0,0 +1,87 @@
+﻿using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Nodes;
+
+namespace ShowCardDmgInRewards.ShowCardDmgInRewardsCode;
+
+[HarmonyPatch]
+public static class CanonicalVarsPatch
+{
+    [HarmonyTargetMethod]
+    public static IEnumerable<MethodBase> TargetMethods()
+    {
+        return (from type in AccessTools.AllTypes()
+                where typeof(CardModel).IsAssignableFrom(type)
+                select type.GetNestedType("<>c", BindingFlags.NonPublic | BindingFlags.Public)).Where(t => t != null)
+            .SelectMany(
+                nested => nested.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public),
+                (nested, m) => new { nested, m })
+            .Where(t => t.m.Name.Contains("<get_CanonicalVars>b__"))
+            .Select(t => t.m);
+    }
+
+    public static IEnumerable<CardModel> GetCards(CardModel card)
+    {
+        if (card.CombatState == null)
+            return NRun.Instance?.GlobalUi.TopBar._player?.Deck.Cards ?? [];
+
+        return card.Owner.PlayerCombatState?.AllCards ?? [];
+    }
+
+    [HarmonyTranspiler]
+    public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    {
+        var list = new List<CodeInstruction>(instructions);
+
+        var getOwner = AccessTools.PropertyGetter(typeof(CardModel), nameof(CardModel.Owner));
+        var getPcs   = AccessTools.PropertyGetter(typeof(Player), nameof(Player.PlayerCombatState));
+        var getAll   = AccessTools.PropertyGetter(typeof(PlayerCombatState), nameof(PlayerCombatState.AllCards));
+
+        var getCards   = AccessTools.Method(typeof(CanonicalVarsPatch), nameof(GetCards));
+
+        for (var i = 0; i < list.Count - 3; i++)
+        {
+            if (!list[i].Calls(getOwner) ||
+                !list[i + 1].Calls(getPcs) ||
+                !list[i + 2].Calls(getAll)) continue;
+            list[i] = new CodeInstruction(OpCodes.Call, getCards);
+            list[i + 1].MakeNop();
+            list[i + 2].MakeNop();
+            break;
+        }
+
+        return list;
+    }
+}
+
+[HarmonyPatch]
+public class CalculatedVarPatches
+{
+   [HarmonyPatch(typeof(CalculatedVar), nameof(CalculatedVar.Calculate))]
+    [HarmonyPrefix]
+    public static bool CalculatedVarPrefix(CalculatedVar __instance, Creature target, ref decimal __result)
+    {
+        if (__instance._multiplierCalc == null 
+            || __instance._owner == null) return true;
+        var card = (CardModel)__instance._owner;
+        if (card.CombatState != null) return true;
+        var num = __instance._multiplierCalc(card, target);
+        __result = __instance.GetBaseVar().BaseValue + __instance.GetExtraVar().BaseValue * num;
+        return false;
+    }
+}
+
+public static class Extensions
+{
+    public static void MakeNop(this CodeInstruction code)
+    {
+        code.opcode = OpCodes.Nop;
+        code.operand = null;
+    }
+}
